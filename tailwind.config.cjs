150e0921f31309d4e419fae0d5ec9a9594b436c6
Sergio Rey <reysergio383@gmail.com>
2023-01-14 02:14:20 -0500

    finish the style of the page index

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,44 +1,27 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
-const colors = require('tailwindcss/colors');
 
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			colors: {
-				primary: colors.blue,
-				secondary: colors.pink,
+				'dark-bg': '#111111',
 			},
 			fontFamily: {
 				sans: ["'InterVariable'", ...defaultTheme.fontFamily.sans],
+			},
+			animation: {
+				'move-bg': 'gradient 10s ease infinite',
+			},
+			keyframes: {
+				gradient: {
+					'0%': { 'background-position': '0 50%' },
+					'50%': { 'background-position': '100% 50%' },
+					'100%': { 'background-position': '0 50%' },
+				},
 			},
 		},
 	},
 	plugins: [require('@tailwindcss/typography')],
 	darkMode: 'class',
 };
-
-/* 
-
-  Alternative tailwind.config.js
-  
-  NOTE: Add this fonts to <head>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;700&display=swap" rel="stylesheet" />
-*/
-
-// module.exports = {
-//   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
-//   theme: {
-//     extend: {
-//       colors: {
-//         primary: colors.cyan,
-//         secondary: colors.lime,
-//       },
-//       fontFamily: {
-//         sans: ["'Nunito'", ...defaultTheme.fontFamily.sans],
-//       },
-//     },
-//   },
-//   plugins: [require("@tailwindcss/typography")],
-//   darkMode: "class",
-// };
